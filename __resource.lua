48eef4acf87032d7e8f582e9534ba1ff29642bbd
clementinise <35346472+clementinise@users.noreply.github.com>
2020-04-12 18:12:39 +0200

    fr.lua locale added

--- __resource.lua
+++ __resource.lua
@@ -1,20 +1,22 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Repairkit'
-version '1.0.2'
+version '2.0'
 
 client_scripts {
 	'@es_extended/locale.lua',
 	'client/main.lua',
+	'locales/fr.lua',
 	'locales/en.lua',
-	'locales/sv.lua',
+	'locales/vs.lua',
 	'config.lua'
 }
 
 server_scripts {
 	'@es_extended/locale.lua',
 	'server/main.lua',
+	'locales/fr.lua',
 	'locales/en.lua',
-	'locales/sv.lua',
+	'locales/vs.lua',
 	'config.lua'
 }
