9ca2c1212a45e1de55e6b734fb072b41856b32fb
clementinise <35346472+clementinise@users.noreply.github.com>
2020-04-12 18:18:04 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config						= {}
 Config.InfiniteRepairs		= false -- Should one repairkit last forever?
-Config.RepairTime			= 15 -- In seconds, how long should a repair take?
+Config.RepairTime			  = 15 -- In seconds, how long should a repair take?
 Config.IgnoreAbort			= true -- Remove repairkit from inventory even if user aborts repairs?
 Config.AllowMecano			= false -- Allow mechanics to use this repairkit?
 
-Config.Locale				= 'en'
+Config.Locale				= 'en' -- "fr" for French, "sv" for Sweden
