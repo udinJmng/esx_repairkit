3e4b7f418364f57ab0282a174866dbd3fa8498a2
clementinise <35346472+clementinise@users.noreply.github.com>
2020-04-12 18:04:21 +0200

    French translation added

---
+++ locales/fr.lua
@@ -0,0 +1,8 @@
+Locales ['fr'] = {
+	['used_kit']					= 'Vous avez utilisé ~y~x1 ~b~Kit de réparation',
+	['must_be_outside']				= 'Vous devez être à l\'extérieur du véhicule !',
+	['no_vehicle_nearby']			= 'Il n\'y a aucun ~r~véhicule ~w~aux alentours',
+	['finished_repair']				= '~g~Vous avez réparé votre véhicule !',
+	['abort_hint']					= 'Appuyez sur ~INPUT_VEH_DUCK~ pour annuler',
+	['aborted_repair']				= 'Vous avez ~r~abandonné ~w~la réparation',
+}
