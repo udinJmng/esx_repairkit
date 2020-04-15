7636e0c99720365d9f8b2c51e6bf04b257e1d4c1
clementinise <35346472+clementinise@users.noreply.github.com>
2020-04-15 16:30:11 +0200

    RealisticVehicleFailure Option

--- config.lua
+++ config.lua
@@ -1,7 +1,8 @@
-Config						= {}
-Config.InfiniteRepairs		= false -- Should one repairkit last forever?
-Config.RepairTime			  = 15 -- In seconds, how long should a repair take?
-Config.IgnoreAbort			= true -- Remove repairkit from inventory even if user aborts repairs?
-Config.AllowMecano			= false -- Allow mechanics to use this repairkit?
+Config								= {}
+Config.InfiniteRepairs				= false 	-- Should one repairkit last forever?
+Config.RepairTime					= 15 		-- In seconds, how long should a repair take?
+Config.IgnoreAbort					= true 		-- Remove repairkit from inventory even if user aborts repairs?
+Config.AllowMecano					= false 	-- Allow mechanics to use this repairkit?
+Config.RealisticVehicleFailure		= true 		-- Set to true if you are using the resource "RealisticVehicleFailure"
 
-Config.Locale				= 'en' -- "fr" for French, "sv" for Sweden
+Config.Locale						= 'fr'
