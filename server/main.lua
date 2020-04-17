72fbe75f5db1391c89da143a73cd49ce5f3b1d74
clementinise <35346472+clementinise@users.noreply.github.com>
2020-04-17 16:47:28 +0200

    TyreKit Added and many other changes (See Changelog 3.0)

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,29 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+-- Update Checker
+local CurrentVersion = '3.0'
+
+PerformHttpRequest('https://raw.githubusercontent.com/clementinise/esx_repairkit/master/version', function(Error, NewestVersion, Header)
+		print('\n')
+		print('########################')
+		print('## RepairKit Resource')
+		print('##')
+		print('## Current Version: ' .. CurrentVersion)
+		print('## Newest Version: ' .. NewestVersion)
+		print('##')
+		if CurrentVersion ~= NewestVersion then
+			print('## Outdated')
+			print('## Please check the GitHub and download the last update')
+			print('## https://github.com/clementinise/esx_repairkit/releases/latest')
+			print('########################')
+		else
+			print('## Up to date!')
+			print('########################')
+		end
+		print('\n')
+end)
+
 -- Make the kit usable!
 ESX.RegisterUsableItem('repairkit', function(source)
 	local _source = source
@@ -15,6 +38,30 @@ ESX.RegisterUsableItem('repairkit', function(source)
 	end
 end)
 
+ESX.RegisterUsableItem('tyrekit', function(source)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	
+	if Config.AllowMecano then
+		TriggerClientEvent('tyrekit:onUse', _source)
+	else
+		if xPlayer.job.name ~= 'mecano' then
+			TriggerClientEvent('tyrekit:onUse', _source)
+		end
+	end
+end)
+
+RegisterNetEvent('esx_repairkit:removeTyreKit')
+AddEventHandler('esx_repairkit:removeTyreKit', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+
+	if not Config.InfiniteRepairsTyreKit then
+		xPlayer.removeInventoryItem('tyrekit', 1)
+		TriggerClientEvent(_U('used_tyrekit'))
+	end
+end)
+
 RegisterNetEvent('esx_repairkit:removeKit')
 AddEventHandler('esx_repairkit:removeKit', function()
 	local _source = source
@@ -22,6 +69,34 @@ AddEventHandler('esx_repairkit:removeKit', function()
 
 	if not Config.InfiniteRepairs then
 		xPlayer.removeInventoryItem('repairkit', 1)
-		TriggerClientEvent('esx:showNotification', _source, _U('used_kit'))
+		TriggerClientEvent(_U('used_kit'))
 	end
+end)
+
+function MecanoOnline()
+
+	local xPlayers = ESX.GetPlayers()
+
+	MechanicConnected = 0
+
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		if xPlayer.job.name == 'mecano' then
+			MechanicConnected = MechanicConnected + 1
+		end
+	end
+
+	SetTimeout(10000, MecanoOnline)
+
+end
+
+MecanoOnline()
+
+RegisterServerEvent('jobonline:get')
+AddEventHandler('jobonline:get', function()
+	local counted = {}
+
+	counted['mecano'] = MechanicConnected
+
+	TriggerClientEvent('jobonline:set', source, counted)
 end)
