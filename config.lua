9fbbce66e03549b30cfc9160fb9b21f31d501878
clementinise <35346472+clementinise@users.noreply.github.com>
2020-12-05 13:14:45 +0100

    Replaced "ExplosionChance" with "DestroyChance" (See Changelog 3.1)

--- config.lua
+++ config.lua
@@ -8,8 +8,8 @@ Config.IgnoreAbort					= true 		-- Remove repairkit from inventory even if user 
 Config.IgnoreTyreAbort				= false 	-- Remove repairkit from inventory even if user aborts repairs?
 Config.AllowMecano					= true	 	-- Allow mechanics to use this repairkit? (If you want this to work set "Config.IfMecaIsOnline" to false, it's a known bug that will be fixed as soon as possible)
 Config.RealisticVehicleFailure		= true 		-- Set to true if you are using the resource "RealisticVehicleFailure"
-Config.ExplosionOnFailedRepair		= true		-- Set to true if you wan't to use "Config.ExplosionChance"
-Config.ExplosionChance				= 100		-- The lower it is, the more it has a chance to make the engine fail and the car explode. (1 = 100%, 2 = 50%, 4 = 25%, 10 = 10%, 100 = 1%, etc)
+Config.DestroyOnFailedRepair		= true		-- Set to true if you wan't to use "Config.DestroyChance"
+Config.DestroyChance				= 100			-- The lower it is, the more it has a chance to destroy the engine. (1 = 100%, 2 = 50%, 4 = 25%, 10 = 10%, 100 = 1%, etc)
 Config.EnableProgressBar			= true		-- If you don't want a progress bar when your player use the item or you don't want to use the resource "progressBars" then set it to false
 
 Config.Locale						= 'fr'
